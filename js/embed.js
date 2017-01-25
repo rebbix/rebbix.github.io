ed804c298f682b68cae61fe1f0aaf52b1764dc8b
Taras Voitovych <tararasik@gmail.com>
2017-01-25 17:56:48 +0200

    Marker fixes

--- js/embed.js
+++ js/embed.js
@@ -1,100 +1,103 @@
-var coordsCards = []
-var $cards = document.querySelectorAll('.card_embed-facebook, .card_embed-youtube, .card_embed-instagram, .card_embed-twitter, .card_embed-image')
+(function() {
+  var coordsCards = []
+  var $cards = document.querySelectorAll('.card_embed-facebook, .card_embed-youtube, .card_embed-instagram, .card_embed-twitter, .card_embed-image')
 
-window.addEventListener('scroll', debounce(arrange, 500))
+  window.addEventListener('scroll', debounce(arrange, 500))
 
-window.addEventListener('load', function() {
-  coordsCards = getCardsCoordinates()
-  // arrange()
-})
+  window.addEventListener('load', function() {
+    coordsCards = getCardsCoordinates()
+    // arrange()
+  })
 
-window.addEventListener('resize', function() {
-  coordsCards = getCardsCoordinates()
-  arrange()
-})
+  window.addEventListener('resize', function() {
+    coordsCards = getCardsCoordinates()
+    arrange()
+  })
 
-function arrange() {
-  var scrollTop = document.body.scrollTop;
+  function arrange() {
+    var scrollTop = document.body.scrollTop;
 
-  for (var i = 0; i < coordsCards.length; i++) {
-    if (coordsCards[i].top >= scrollTop && coordsCards[i].top <= (scrollTop + window.innerHeight)) {
-      if (!$cards[i].dataset.loaded) {
-        $cards[i].dataset.loaded = true;
-        addPost($cards[i]);
+    for (var i = 0; i < coordsCards.length; i++) {
+      if (coordsCards[i].top >= scrollTop && coordsCards[i].top <= (scrollTop + window.innerHeight)) {
+        if (!$cards[i].dataset.loaded) {
+          $cards[i].dataset.loaded = true;
+          addPost($cards[i]);
+        }
       }
     }
   }
-}
 
-function getCardsCoordinates() {
-  var coords = []
-  for (var i = 0; i < $cards.length; i++) {
-    var $card = $cards[i]
-    var rect = $card.getBoundingClientRect()
+  function getCardsCoordinates() {
+    var coords = []
 
-    coords.push({
-      $card: $card
-    , top: rect.top + document.body.scrollTop
-    })
-  }
-  return coords
-}
+    for (var i = 0; i < $cards.length; i++) {
+      var $card = $cards[i]
+      var rect = $card.getBoundingClientRect()
 
-function addPost(card) {
-
-  if (card.className.indexOf('card_embed-instagram') > -1) {
-    var blockquote = document.createElement('blockquote')
-    blockquote.classList.add('instagram-media')
-    blockquote.dataset.instgrmCaptioned = true
-    blockquote.dataset.instgrmVersion = 7
-    var link = document.createElement('a')
-    link.setAttribute('href', card.dataset.embedUrl)
-    blockquote.appendChild(link)
-    card.querySelector('.card__content').appendChild(blockquote)
-    window.instgrm && window.instgrm.Embeds.process()
-    coordsCards = getCardsCoordinates()
+      coords.push({
+        $card: $card
+      , top: rect.top + document.body.scrollTop
+      })
+    }
+    return coords
   }
 
-  else if (card.className.indexOf('card_embed-twitter') > -1) {
-    var blockquote = document.createElement('blockquote')
-    blockquote.classList.add('twitter-tweet')
-    blockquote.dataset.lang = "en"
-    var link = document.createElement('a')
-    link.setAttribute('href', card.dataset.embedUrl)
-    blockquote.appendChild(link)
-    card.querySelector('.card__content').appendChild(blockquote)
-    twttr.widgets.load(card)
-    card.addEventListener('load', function () {
+  function addPost(card) {
+
+    if (card.className.indexOf('card_embed-instagram') > -1) {
+      var blockquote = document.createElement('blockquote')
+      blockquote.classList.add('instagram-media')
+      blockquote.dataset.instgrmCaptioned = true
+      blockquote.dataset.instgrmVersion = 7
+      var link = document.createElement('a')
+      link.setAttribute('href', card.dataset.embedUrl)
+      blockquote.appendChild(link)
+      card.querySelector('.card__content').appendChild(blockquote)
+      window.instgrm && window.instgrm.Embeds.process()
       coordsCards = getCardsCoordinates()
-    })
+    }
+
+    else if (card.className.indexOf('card_embed-twitter') > -1) {
+      var blockquote = document.createElement('blockquote')
+      blockquote.classList.add('twitter-tweet')
+      blockquote.dataset.lang = "en"
+      var link = document.createElement('a')
+      link.setAttribute('href', card.dataset.embedUrl)
+      blockquote.appendChild(link)
+      card.querySelector('.card__content').appendChild(blockquote)
+      twttr.widgets.load(card)
+      card.addEventListener('load', function () {
+        coordsCards = getCardsCoordinates()
+      })
+    }
+
+    else if (card.className.indexOf('card_embed-image') > -1) {
+      var img = document.createElement('img')
+      img.setAttribute('src', card.dataset.embedUrl)
+      card.querySelector('.card__content').appendChild(img)
+    }
+
+    else {
+      var iframe = document.createElement('iframe')
+      iframe.setAttribute('width', card.dataset.embedWidth)
+      iframe.setAttribute('class' , 'facebook_post')
+      iframe.setAttribute('height', card.dataset.embedHeight)
+      iframe.setAttribute('src', card.dataset.embedUrl)
+      card.querySelector('.card__content').appendChild(iframe)
+      iframe.addEventListener('load', function() {
+      coordsCards = getCardsCoordinates()
+      })
+    }
   }
 
-  else if (card.className.indexOf('card_embed-image') > -1) {
-    var img = document.createElement('img')
-    img.setAttribute('src', card.dataset.embedUrl)
-    card.querySelector('.card__content').appendChild(img)
+  function debounce(fn, delay) {
+    var timer = null;
+    return function () {
+      var context = this, args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(function () {
+        fn.apply(context, args);
+      }, delay);
+    };
   }
-
-  else {
-    var iframe = document.createElement('iframe')
-    iframe.setAttribute('width', card.dataset.embedWidth)
-    iframe.setAttribute('class' , 'facebook_post')
-    iframe.setAttribute('height', card.dataset.embedHeight)
-    iframe.setAttribute('src', card.dataset.embedUrl)
-    card.querySelector('.card__content').appendChild(iframe)
-    iframe.addEventListener('load', function() {
-    coordsCards = getCardsCoordinates()
-    })
-  }
-}
-
-function debounce(fn, delay) {
-  var timer = null;
-  return function () {
-    var context = this, args = arguments;
-    clearTimeout(timer);
-    timer = setTimeout(function () {
-      fn.apply(context, args);
-    }, delay);
-  };
-}
+})();
--- js/marker.js
+++ js/marker.js
@@ -1,164 +1,153 @@
-var url = window.location.pathname
-var hideMarkers = ['/approach', '/contacts'].indexOf(url) > -1
-var reverseMarkers = ['/team'].indexOf(url) === -1
-var dynamicMarkers = ['/life'].indexOf(url) > -1
-var yearSeparator = ['/life'].indexOf(url) > -1
+(function() {
+  const HEADER_HEIGHT = 60;
 
-if (!hideMarkers) {
-  var $cards = document.querySelectorAll('.card')
-  var cardsArray = [].slice.call($cards)
-  var coordsAnchors = getAnchorsCoordinates()
+  var url = window.location.pathname;
+  var hideMarkers = ['/approach', '/contacts'].indexOf(url) > -1;
+  var reverseMarkers = ['/team'].indexOf(url) === -1;
+  var replaceLatestYear = url == '/';
 
-  // if (yearSeparator) addYearSeparator()
+  if (!hideMarkers) {
+    var $cards = document.querySelectorAll('.card');
+    var cardsArray = [].slice.call($cards);
+    var coordsAnchors = getAnchorsCoordinates();
 
-  var $marker = document.createElement('div')
-  var $markers = []
+    var $marker = document.createElement('div');
+    var $markers = [];
 
-  $marker.classList.add('marker')
+    $marker.classList.add('marker');
 
-  coordsAnchors.forEach(function (anchor) {
-    var $markerItem = document.createElement('div')
-    $markerItem.classList.add('marker__item')
-    $markerItem.innerHTML = anchor.year
-    $marker.appendChild($markerItem)
-    $markers.push($markerItem)
-  })
-  document.body.appendChild($marker)
-
-  var coordsMarkers = getMarkersCoordinates()
-  document.addEventListener('DOMContentLoaded', arrange)
-  window.addEventListener('scroll', arrange)
-  window.addEventListener('resize', function () {
-    coordsAnchors = getAnchorsCoordinates()
-    arrange()
-  })
-
-  if (dynamicMarkers) initContentObserver()
-  initMarkerClick()
-
-
-
-  function arrange() {
-    if (dynamicMarkers) {
-      coordsAnchors = getAnchorsCoordinates()
-    }
-
-    coordsMarkers.forEach(function (marker, i) {
-      var anchor = coordsAnchors.find(function(card) {
-        return card.year === marker.year
-      })
-
-      if (!anchor) return
-
-      const contentTop = i * 35 + 60
-      const initialMarkerTop = marker.top
-      const anchorTop = anchor.top - 30 - document.body.scrollTop
-
-      if ((anchorTop > contentTop) && (anchorTop < initialMarkerTop)) {
-        marker.$marker.classList.add('marker__item_active')
-        marker.$marker.classList.remove('marker__item_top')
-        marker.$marker.style.top = anchor.top - 30 + 'px'
-      } else if (anchorTop < contentTop) {
-        marker.$marker.classList.remove('marker__item_active')
-        marker.$marker.classList.add('marker__item_top')
-      } else {
-        marker.$marker.classList.remove('marker__item_active')
-        marker.$marker.style.top = '100%'
-      }
-    })
-  }
-
-  function getAnchorsCoordinates() {
-    var coords = {}
-
-    for (var i = 0; i < $cards.length; i++) {
-      var year = $cards[i].dataset.year
-
-      if (coords[year]) continue
-
-      var rect = $cards[i]
-        .querySelector('.card__wrap')
-        .getBoundingClientRect()
-
-      coords[year] = {
-        top: rect.top + document.body.scrollTop,
-        year: year
-      }
-    }
-
-    var anchorsCoords = Object.keys(coords).map(function (i) {
-      return coords[i]
-    })
-
-    if (reverseMarkers) {
-      anchorsCoords = anchorsCoords.reverse()
-    }
-
-    return anchorsCoords
-  }
-
-  function getMarkersCoordinates() {
-    var coords = []
-    for (var i = 0; i < $markers.length; i++) {
-      var $marker = $markers[i]
-      var year = $marker.innerHTML
-      var rect = $marker.getBoundingClientRect()
-
-      coords.push({
-        $marker: $marker,
-        top: rect.top,
-        year: year,
-      })
-    }
-    return coords
-  }
-
-  function initMarkerClick() {
-    coordsMarkers.forEach(function (marker) {
-      marker.$marker.addEventListener('click', function () {
-        var anchor = cardsArray.find(function (card) {
-          return card.dataset.year === marker.year
-        })
-
-        var anchorTop = anchor.querySelector('.card__wrap').getBoundingClientRect().top
-        var top = anchorTop + document.body.scrollTop
-
-        scrollTo(document.body, top - 60, 500) // 60 - header height
-        window.scrollTo(top - 60) // hack to fire scroll event
-      })
-    })
-  }
-
-  function initContentObserver() {
-    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-    var observer = new MutationObserver(function () {
-      arrange()
+    var years = coordsAnchors.map(function(anchor) {
+      return parseInt(anchor.year);
     });
 
-    observer.observe(document.querySelector('.content'), {
-      subtree: true,
-      childList: true,
-    })
-  }
-
-  function addYearSeparator() {
-    var content = document.querySelector('.content')
+    var maxYear = Math.max.apply(null, years);
 
     coordsAnchors.forEach(function (anchor) {
-      var card = cardsArray.find(function (card) {
-        return card.dataset.year === anchor.year
+      var $markerItem = document.createElement('div');
+      $markerItem.classList.add('marker__item');
+      $markerItem.innerHTML = anchor.year;
+      $markerItem.dataset.year = anchor.year;
+      if (replaceLatestYear && anchor.year == maxYear) {
+        $markerItem.innerHTML = 'Now';
+      }
+
+      $marker.appendChild($markerItem);
+      $markers.push($markerItem);
+    });
+    document.body.appendChild($marker);
+
+    var coordsMarkers = getMarkersCoordinates();
+    arrange()
+
+    window.addEventListener('scroll', function () {
+      coordsAnchors = getAnchorsCoordinates();
+      arrange();
+    });
+    window.addEventListener('resize', function () {
+      coordsAnchors = getAnchorsCoordinates();
+      arrange();
+    });
+
+    initMarkerClick();
+
+    function arrange() {
+      coordsMarkers.forEach(function (marker, i) {
+        var anchor = coordsAnchors.find(function(card) {
+          return card.year === marker.year;
+        });
+
+        if (!anchor) return;
+
+        const contentTop = i * 35 + HEADER_HEIGHT;
+        const initialMarkerTop = marker.top;
+        const anchorTop = anchor.top - 30 - document.body.scrollTop;
+
+        if ((anchorTop > contentTop) && (anchorTop < initialMarkerTop)) {
+          marker.$marker.classList.add('marker__item_active');
+          marker.$marker.classList.remove('marker__item_top');
+          marker.$marker.classList.remove('marker__item_top-active');
+
+          marker.$marker.style.top = anchor.top - 30 + 'px';
+        } else if (anchorTop < contentTop) {
+          marker.$marker.classList.remove('marker__item_active');
+          marker.$marker.classList.add('marker__item_top');
+          marker.$marker.classList.add('marker__item_top-active');
+
+          if (
+              marker.$marker.nextSibling &&
+              (marker.$marker.nextSibling.classList.contains('marker__item_top') ||
+              marker.$marker.nextSibling.classList.contains('marker__item_active'))
+          ) {
+            marker.$marker.classList.remove('marker__item_top-active');
+          }
+
+        } else {
+          marker.$marker.classList.remove('marker__item_active');
+          marker.$marker.classList.remove('marker__item_top-active');
+          marker.$marker.style.top = '100%';
+        }
+      })
+    }
+
+    function getAnchorsCoordinates() {
+      var coords = {};
+
+      for (var i = 0; i < $cards.length; i++) {
+        var year = $cards[i].dataset.year;
+
+        if (coords[year]) continue;
+
+        var rect = $cards[i]
+          .getBoundingClientRect();
+
+        coords[year] = {
+          top: rect.top + document.body.scrollTop,
+          year: year
+        }
+      }
+
+      var anchorsCoords = Object.keys(coords).map(function (i) {
+        return coords[i];
       })
 
-      var separator = document.createElement('div')
-      separator.classList.add('card', 'card_year')
-      separator.dataset.year = anchor.year
-      separator.innerHTML = '<div class="card__wrap">' + anchor.year + '</div>'
+      if (reverseMarkers) {
+        anchorsCoords = anchorsCoords.reverse();
+      }
 
-      content.insertBefore(separator, card)
-    })
+      return anchorsCoords;
+    }
 
-    $cards = document.querySelectorAll('.card')
-    cardsArray = [].slice.call($cards)
-    coordsAnchors = getAnchorsCoordinates()
+    function getMarkersCoordinates() {
+      var coords = [];
+      for (var i = 0; i < $markers.length; i++) {
+        var $marker = $markers[i];
+        var year = $marker.dataset.year;
+        var rect = $marker.getBoundingClientRect();
+
+        coords.push({
+          $marker: $marker,
+          top: rect.top,
+          year: year,
+        })
+      }
+
+      return coords;
+    }
+
+    function initMarkerClick() {
+      coordsMarkers.forEach(function (marker) {
+        marker.$marker.addEventListener('click', function () {
+          var anchor = cardsArray.find(function (card) {
+            return card.dataset.year === marker.year;
+          });
+
+          var anchorTop = anchor.querySelector('.card__wrap').getBoundingClientRect().top;
+          var top = anchorTop + document.body.scrollTop;
+
+          scrollTo(document.body, top - HEADER_HEIGHT, 500); // 60 - header height
+          window.scrollTo(top - HEADER_HEIGHT); // hack to fire scroll event
+        })
+      })
+    }
   }
-}
+})();
