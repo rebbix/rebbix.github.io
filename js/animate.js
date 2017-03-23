f8f78e0cc8c4a2c255ea53d513d8363a2f0722d7
Max Vyznyuk <mvyznyuk@gmail.com>
2017-03-23 19:01:28 +0200

    Fix card appearing height

--- js/animate.js
+++ js/animate.js
@@ -9,7 +9,7 @@ function isInView() {
     var elementBounds = element.getBoundingClientRect();
     var elementTop = elementBounds.top;
 
-    if (elementTop <= windowHeight - windowHeight * 0.3) {
+    if (elementTop <= windowHeight - windowHeight * 0.15) {
       element.classList.add('card_in-view');
     } else {
       element.classList.remove('card_in-view');
