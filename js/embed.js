5553d9778badcab2d93d82d61ce4ef6df425df08
Max Vyznyuk <mvyznyuk@gmail.com>
2017-03-28 19:11:36 +0300

    Move labels down, add separators (life page)

--- js/embed.js
+++ js/embed.js
@@ -3,7 +3,7 @@ function addPost(card) {
   iframe.setAttribute('width', '100%')
   iframe.setAttribute('height', '300px')
   iframe.setAttribute('src', card.dataset.videoembed)
-  card.querySelector('.card__content').appendChild(iframe)
+  card.querySelector('.card__content').insertBefore(iframe, card.querySelector('.card__label'));
   iframe.addEventListener('load', function() {
     card.dataset.loaded = true;
   })
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
     cards.forEach((card, index) => {
       var cardWidth = card.dataset.videoembed ? 47 : Math.floor(Math.random() * 20 + 25);
       var cardMargin = Math.floor(Math.random() * (47 - cardWidth));
-      var cardMarginBottom = Math.floor(Math.random() * 3 + 5);
+      var cardMarginBottom = Math.floor(Math.random() * 7 + 8);
       var cardMarginTop = -Math.floor(Math.random() * 3 + 2);
 
       var cardSide = (index % 2) ? 'card_right' : 'card_left';
