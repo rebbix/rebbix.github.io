9689a908887cff5cf3443df95d94ceaa3dc76b58
Max Vyz <mvyznyuk@gmail.com>
2018-10-13 13:06:32 +0300

    Improve google map appearance

--- js/contacts.js
+++ js/contacts.js
@@ -1,108 +1,27 @@
 function initContactsMap() {
   var coords = new google.maps.LatLng(49.831792,24.044215);
-  var myOptions = {
+  var mapOptions = {
     zoom: 17,
     center: coords,
-  };
-
-  var map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
-  map.setOptions({
+    streetViewControl: false,
+    mapTypeControl: false,
+    fullscreenControl: false,
     styles: [
       {
-        elementType: 'geometry',
-        stylers: [{color: '#f5f5f5'}]
-      },
-      {
-        elementType: 'labels.icon',
-        stylers: [{visibility: 'off'}]
-      },
-      {
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#616161'}]
-      },
-      {
-        elementType: 'labels.text.stroke',
-        stylers: [{color: '#f5f5f5'}]
-      },
-      {
-        featureType: 'administrative.land_parcel',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#bdbdbd'}]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'geometry',
-        stylers: [{color: '#eeeeee'}]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#757575'}]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'geometry',
-        stylers: [{color: '#e5e5e5'}]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#9e9e9e'}]
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [{color: '#ffffff'}]
-      },
-      {
-        featureType: 'road.arterial',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#757575'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [{color: '#dadada'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#616161'}]
-      },
-      {
-        featureType: 'road.local',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#9e9e9e'}]
-      },
-      {
-        featureType: 'transit.line',
-        elementType: 'geometry',
-        stylers: [{color: '#e5e5e5'}]
-      },
-      {
-        featureType: 'transit.station',
-        elementType: 'geometry',
-        stylers: [{color: '#eeeeee'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'geometry',
-        stylers: [{color: '#c9c9c9'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#9e9e9e'}]
+        featureType: "poi",
+        elementType: "labels",
+        stylers: [
+          { visibility: "off" }
+        ]
       }
     ]
-  })
+  };
 
-  var marker = new google.maps.Marker({map: map, position: coords});
-  var infoWindow = new google.maps.InfoWindow({
-    content: "<b>Rebbix</b><br/>Levytskogo str. 84<br/>"
-  });
+  var map = new google.maps.Map(document.getElementById("gmap_canvas"), mapOptions);
+  var marker = new google.maps.Marker({ map, position: coords, icon: '/img/map_pin.svg' });
+  var infoWindow = new google.maps.InfoWindow({ content: "<b>Rebbix</b><br/>Levytskogo str. 84<br/>" });
 
   google.maps.event.addListener(marker, "click", function() {
-    infoWindow.open(map,marker);
+    infoWindow.open(map, marker);
   });
 }
