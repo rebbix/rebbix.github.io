e318ef439386c11f76ef151c155a0da0b36cf8c8
Serhiy Oplakanets <serhiy@oplakanets.com>
2018-10-16 15:33:51 +0300

    Format change

--- js/contacts.js
+++ js/contacts.js
@@ -19,7 +19,7 @@ function initContactsMap() {
 
   var map = new google.maps.Map(document.getElementById("gmap_canvas"), mapOptions);
   var marker = new google.maps.Marker({ map, position: coords, icon: '/img/map_pin.svg' });
-  var infoWindow = new google.maps.InfoWindow({ content: "<b>Rebbix</b><br/>Levytskogo str. 84<br/>" });
+  var infoWindow = new google.maps.InfoWindow({ content: "<b>Rebbix</b><br/>Levyts’koho St 84<br/>" });
 
   google.maps.event.addListener(marker, "click", function() {
     infoWindow.open(map, marker);
